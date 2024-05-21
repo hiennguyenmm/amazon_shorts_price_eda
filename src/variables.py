e62ed4a3ef9e3957d7bd527c3f6f44834b0dda05
hiennguyenmm <minhhiennguyen612@gmail.com>
2024-05-21 13:39:14 +0700

    edit ratings_class

--- src/variables.py
+++ src/variables.py
@@ -3,7 +3,7 @@ output_class = "s-result-item"
 itemname_class = "a-size-base-plus a-color-base a-text-normal"
 price_class = "a-offscreen"
 no_of_ratings_class = "a-size-base s-underline-text"
-ratings_class = "a-icon a-icon-star-small a-star-small-4 aok-align-bottom"
+ratings_class = "a-icon-alt"
 bought_last_month_class = "a-size-base a-color-secondary"
 text_box_id = "twotabsearchtextbox"
 # xpath_string = f'//*[@id="search"]/div[1]/div[1]/div/span[1]/div[1]/div[63]/div/div/span/a[{page_number}]'
