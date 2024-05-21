53378280b428f0b2002ea126f5b6a3ae4974df2f
hiennguyenmm <minhhiennguyen612@gmail.com>
2024-05-21 13:38:38 +0700

    include bought_last_month in select

--- sql/cast_data_type_df.sql
+++ sql/cast_data_type_df.sql
@@ -2,6 +2,7 @@ select
     item_name,
     cast(replace(price, '$', '') as float) as price,
     cast(replace(no_of_ratings, ',', '') as integer) as no_of_ratings,
+    cast(replace(ratings, ' out of 5 stars', '') as float) as ratings,
     bought_last_month
 from
     df
