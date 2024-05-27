96a47e50c613dd7e4bfb1d396d940cd61595bcff
hiennguyenmm <minhhiennguyen612@gmail.com>
2024-05-27 23:29:49 +0700

    preprocess field bought last month

--- sql/cast_data_type_df.sql
+++ sql/cast_data_type_df.sql
@@ -3,7 +3,10 @@ select
     cast(replace(price, '$', '') as float) as price,
     cast(replace(no_of_ratings, ',', '') as integer) as no_of_ratings,
     cast(replace(ratings, ' out of 5 stars', '') as float) as ratings,
-    bought_last_month
+    case
+        when bought_last_month not like '% bought in past month' then null
+        else cast(replace(bought_last_month, '+ bought in past month', '') as integer)
+    end as bought_last_month
 from
     df
 where
